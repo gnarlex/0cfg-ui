09e46cafee281ed1a1545e23f54a5ee27fe1074f
romfrolov <justcallmefrolov@gmail.com>
2021-12-03 10:35:43 +0300

    Add Storybook for development

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+    stories: [
+        '../packages/ui/browser/src/__stories__/stories.mdx',
+        '../packages/ui/browser/src/__stories__/*.stories.@(js|jsx|ts|tsx)'
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials'
+    ],
+    babel: async (options) => {
+        options.plugins.push(require.resolve('babel-plugin-transform-typescript-metadata'));
+
+        return options;
+    }
+}
